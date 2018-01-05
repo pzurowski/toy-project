7c28c451276f9ee8f11966723aeaa408e41b026b
Paweł Żurowski <pawel.zurowski@decerto.pl>
2018-01-05 11:58:27 +0100

    feat(xkcd-rng): implementation

--- src/app/xkcd-rng.service.ts
+++ src/app/xkcd-rng.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {RandomNumberGenerator} from './random-number-generator';
 
 @Injectable()
-export class XkcdRngService {
+export class XkcdRngService implements RandomNumberGenerator {
+  readonly infoUrl = 'https://xkcd.com/221/';
+  readonly name = 'XKCD Random Number Generator';
 
-  constructor() { }
+  reset() {
+    return null;
+  }
 
+  next(state: any = null) {
+    return {state, value: 4};
+  }
 }
+
