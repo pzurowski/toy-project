2ddae4b09da8f3a73adb88e770d911f3895e5530
Paweł Żurowski <pawel.zurowski@decerto.pl>
2018-01-05 12:32:17 +0100

    feat(counter): implementation

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
 
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-counter',
-  template: `
-    <p>
-      counter works!
-    </p>
-  `,
-  styles: []
+  template: `{{ tick | async }}`,
 })
-export class CounterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class CounterComponent {
+  @Input() counter = 5;
+  @Input() delay = 90;
+  @Output() finish = new EventEmitter<boolean>();
+  @Output() tick = new EventEmitter<number>();
+  public start(): void {
+    this.count(0);
   }
-
+  private count(n: number): void {
+    this.tick.emit(n);
+    if (n >= this.counter) {
+      this.finish.emit(true);
+    } else {
+      setTimeout(() => this.count(n + 1), this.delay);
+    }
+  }
 }
+
